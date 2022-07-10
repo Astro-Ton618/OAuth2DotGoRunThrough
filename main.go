380a381270e964697af550ba1052358547521a72
Massimiliano Falsini <falsinimassimiliano@gmail.com>
2022-07-10 22:16:00 +0200

    implemented token authorization

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	client_id := ""
-
+	client_secret := ""
 	fmt.Println(Generate_permission_url(client_id))
+	fmt.Println(Generate_token_authorization(client_id, client_secret, "browser_authorization_code"))
 }
--- oauth2.go
+++ oauth2.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"net/url"
 )
 
@@ -9,6 +13,12 @@ type permission_url struct {
 	redirect_uri  string
 	response_type string
 	scope         string
+}
+
+type token_authorization_response struct {
+	refresh_token                   string
+	access_token                    string
+	access_token_expiration_seconds string
 }
 
 func Generate_permission_url(client_id string) string {
@@ -22,4 +32,43 @@ func Generate_permission_url(client_id string) string {
 	url := "https://accounts.google.com/o/oauth2/auth?" + "client_id=" + params.client_id + "&redirect_uri=" + params.redirect_uri + "&response_type=" + params.response_type + "&scope=" + params.scope
 
 	return (url)
+}
+
+func Generate_token_authorization(client_id string, client_secret string, authorization_code string) token_authorization_response {
+	values := map[string]string{"client_id": client_id, "client_secret": client_secret, "code": authorization_code, "redirect_uri": "urn:ietf:wg:oauth:2.0:oob", "grant_type": "authorization_code"}
+
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	respo, err := http.Post("https://accounts.google.com/o/oauth2/token", "application/json", bytes.NewBuffer(json_data))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp := map[string]string{}
+	json.NewDecoder(respo.Body).Decode(&resp)
+
+	res := token_authorization_response{
+		refresh_token:                   "",
+		access_token:                    "",
+		access_token_expiration_seconds: "",
+	}
+
+	if resp["expires_in"] == "" {
+		res = token_authorization_response{
+			refresh_token:                   resp["refresh_token"],
+			access_token:                    resp["access_token"],
+			access_token_expiration_seconds: "3600",
+		}
+	} else {
+		res = token_authorization_response{
+			refresh_token:                   resp["refresh_token"],
+			access_token:                    resp["access_token"],
+			access_token_expiration_seconds: resp["expires_in"],
+		}
+	}
+
+	return (res)
 }
